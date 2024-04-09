8b5e7e7e393baaba3cb44008095662ae3417212c
Hyeon Kim <simnalamburt@gmail.com>
2024-04-10 08:40:19 +0900

    build.zig: Produce smaller binary

--- build.zig
+++ build.zig
@@ -5,22 +5,24 @@ pub fn build(b: *std.Build) void {
         .name = "input-source-aarch64",
         .root_source_file = .{ .path = "main.zig" },
         .target = std.zig.CrossTarget{ .cpu_arch = .aarch64, .os_tag = .macos },
-        // TODO: standardOptimizeOption
+        .optimize = .ReleaseSmall,
+        .single_threaded = true,
     });
 
     const x86_64 = b.addExecutable(.{
         .name = "input-source-x86_64",
         .root_source_file = .{ .path = "main.zig" },
         .target = std.zig.CrossTarget{ .cpu_arch = .x86_64, .os_tag = .macos },
-        // TODO: standardOptimizeOption
+        .optimize = .ReleaseSmall,
+        .single_threaded = true,
     });
 
-    const lipo = b.addSystemCommand(&.{ "lipo", "-create" });
+    const lipo = b.addSystemCommand(&.{ "lipo", "-create", "-output" });
+    const universal = lipo.addOutputFileArg("input-source");
     lipo.addArtifactArg(aarch64);
     lipo.addArtifactArg(x86_64);
-    lipo.addArg("-output");
-    const universal = lipo.addOutputFileArg("input-source");
 
-    const install = b.addInstallBinFile(universal, "input-source");
-    b.default_step.dependOn(&install.step);
+    b.default_step.dependOn(&b.addInstallArtifact(aarch64, .{}).step);
+    b.default_step.dependOn(&b.addInstallArtifact(x86_64, .{}).step);
+    b.default_step.dependOn(&b.addInstallBinFile(universal, "input-source").step);
 }
