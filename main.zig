875af71a6a26eacab1f1ee57d02eb4c0c5e2a31a
Hyeon Kim <simnalamburt@gmail.com>
2024-10-30 03:20:47 +0900

    main.zig: Properly parse argv

--- main.zig
+++ main.zig
@@ -5,59 +5,121 @@ const c = @cImport({
     @cInclude("Carbon/Carbon.h");
 });
 
+const Input = union(enum) {
+    current: struct {
+        localized_name: bool,
+    },
+    list: struct {
+        localized_name: bool,
+    },
+    set: struct {
+        localized_name: bool,
+        input_source: [*:0]const u8,
+    },
+};
+
 pub fn main() !void {
+    const input = parseArgv(std.os.argv);
+
+    // Initialize the GeneralPurposeAllocator
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     defer _ = gpa.deinit();
-
     const allocator = gpa.allocator();
 
-    const argv = std.os.argv;
-    if (argv.len == 2 and strEqZ(argv[1], "current")) {
-        const source = c.TISCopyCurrentKeyboardInputSource();
-        defer c.CFRelease(source);
-
-        try printID(allocator, source);
-    } else if (argv.len == 2 and strEqZ(argv[1], "list")) {
-        const list = try querySources(.{
-            c.kTISPropertyInputSourceCategory,        c.kTISCategoryKeyboardInputSource,
-            c.kTISPropertyInputSourceIsSelectCapable, c.kCFBooleanTrue,
-            c.kTISPropertyInputSourceIsEnabled,       c.kCFBooleanTrue,
-        }) orelse return;
-        defer c.CFRelease(list);
-
-        for (0..len(list)) |i| {
-            const source = getRetained(list, i);
+    // TODO: opt.localized_name
+    switch (input) {
+        .current => {
+            const source = c.TISCopyCurrentKeyboardInputSource();
             defer c.CFRelease(source);
 
             try printID(allocator, source);
-        }
-    } else if (argv.len == 3 and strEqZ(argv[1], "set")) {
-        const id = try fromStringCreate(argv[2]);
-        defer c.CFRelease(id);
+        },
+        .list => {
+            const list = try querySources(.{
+                c.kTISPropertyInputSourceCategory,        c.kTISCategoryKeyboardInputSource,
+                c.kTISPropertyInputSourceIsSelectCapable, c.kCFBooleanTrue,
+                c.kTISPropertyInputSourceIsEnabled,       c.kCFBooleanTrue,
+            }) orelse return;
+            defer c.CFRelease(list);
 
-        const list = try querySources(.{
-            c.kTISPropertyInputSourceID, id,
-        }) orelse {
-            std.debug.print("input-source: No such input source exists.\n", .{});
-            std.posix.exit(1);
-        };
-        defer c.CFRelease(list);
+            for (0..len(list)) |i| {
+                const source = getRetained(list, i);
+                defer c.CFRelease(source);
 
-        if (len(list) != 1) {
-            std.debug.print("input-source: Multiple input sources found with the given ID.\n", .{});
-            std.posix.exit(1);
-        }
+                try printID(allocator, source);
+            }
+        },
+        .set => |*opt| {
+            const id = try fromStringCreate(opt.input_source);
+            defer c.CFRelease(id);
 
-        const source = getRetained(list, 0) orelse return error.Unreachable; // TODO: error
-        defer c.CFRelease(source);
+            const list = try querySources(.{
+                c.kTISPropertyInputSourceID, id,
+            }) orelse {
+                std.debug.print("input-source: No such input source exists.\n", .{});
+                std.posix.exit(1);
+            };
+            defer c.CFRelease(list);
 
-        const status = c.TISSelectInputSource(source);
-        if (status != 0) {
-            std.debug.print("input-source: Could not change input language (OSStatus = {})", .{status});
-            std.posix.exit(1);
-        }
-    } else {
+            if (len(list) != 1) {
+                std.debug.print("input-source: Multiple input sources found with the given ID.\n", .{});
+                std.posix.exit(1);
+            }
+
+            const source = getRetained(list, 0) orelse return error.Unreachable; // TODO: error
+            defer c.CFRelease(source);
+
+            const status = c.TISSelectInputSource(source);
+            if (status != 0) {
+                std.debug.print("input-source: Could not change input language (OSStatus = {})", .{status});
+                std.posix.exit(1);
+            }
+        },
+    }
+}
+
+fn parseArgv(args: [][*:0]const u8) Input {
+    if (args.len < 2) return showHelpThenExit();
+
+    return if (strEqZ(args[1], "current"))
+        parseCurrent(args[2..])
+    else if (strEqZ(args[1], "list"))
+        parseList(args[2..])
+    else if (strEqZ(args[1], "set"))
+        parseSet(args[2..])
+    else
         showHelpThenExit();
+}
+
+fn parseCurrent(args: [][*:0]const u8) Input {
+    return if (args.len == 0)
+        Input{ .current = .{ .localized_name = false } }
+    else if (args.len == 1 and strEqZ(args[0], "--localized-name"))
+        Input{ .current = .{ .localized_name = true } }
+    else
+        showHelpThenExit();
+}
+
+fn parseList(args: [][*:0]const u8) Input {
+    return if (args.len == 0)
+        Input{ .list = .{ .localized_name = false } }
+    else if (args.len == 1 and strEqZ(args[0], "--localized-name"))
+        Input{ .list = .{ .localized_name = true } }
+    else
+        showHelpThenExit();
+}
+
+fn parseSet(args: [][*:0]const u8) Input {
+    if (args.len < 1) return showHelpThenExit();
+
+    if (strEqZ(args[0], "--localized-name")) {
+        if (args.len != 2) return showHelpThenExit();
+
+        return Input{ .set = .{ .localized_name = true, .input_source = args[1] } };
+    } else {
+        if (args.len != 1) return showHelpThenExit();
+
+        return Input{ .set = .{ .localized_name = false, .input_source = args[0] } };
     }
 }
 
@@ -65,16 +127,23 @@ fn showHelpThenExit() noreturn {
     std.debug.print(
         \\tiny input source manager
         \\
-        \\Usage: input-source <command>
+        \\Usage: input-source <command> [--localized-name] [<args>]
         \\
         \\Commands:
-        \\  current       Show current input source
-        \\  list          List available input sources
-        \\  set <Source>  Change input source to <source>
+        \\  current           Show current input source
+        \\  list              List available input sources
+        \\  set <Source>      Change input source to <source>
+        \\
+        \\Options:
+        \\  --localized-name  Use LocalizedName instead of InputSourceID
         \\
         \\Examples:
+        \\  input-source current
+        \\  input-source current --localized-name
         \\  input-source list
+        \\  input-source list --localized-name
         \\  input-source set com.apple.keylayout.ABC
+        \\  input-source set --localized-name ABC
         \\
     , .{});
     std.posix.exit(1);
